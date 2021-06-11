25191f319bd5e5be07032e618a31903feaa6d7fb
ShuumaIwamoto <iwa06ki12da@gmail.com>
2021-06-12 00:51:06 +0900

    passwordの再設定画面を作成(仮)

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -27,6 +27,12 @@
                             </div>
 
                             <input type="hidden" name="remember" id="remember" value="on">
+
+                            <!-- 追記 -->
+                            <div class="text-left">
+                                <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+                            </div>
+                            <!-- -- -->
                             <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
                         </form>
                         <div class="mt-0">
